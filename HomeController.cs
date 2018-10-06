94ba79c363bc32fed93f0d196e4a8c34d4abe47b
Gabriel4420 <37821374+Gabriel4420@users.noreply.github.com>
2018-10-06 15:20:03 -0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace eCommerceWeb.Controllers.HomeController
+{
+	public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult LayoutInicial()
+        {
+			Session["Cliente"] = null;
+			return View();
+        }
+
+      
+        
+    }
+}
---
+++ HomeLoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerceWeb.Controllers.HomeController
+{
+    public class HomeLoginController : Controller
+    {
+        // GET: HomeLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
