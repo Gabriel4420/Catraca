dd1722c9df5244505c1a4eb9270f916a5011fc43
Gabriel4420 <37821374+Gabriel4420@users.noreply.github.com>
2018-10-06 15:19:26 -0300

    Add files via upload

---
+++ CategoriaController.cs
@@ -0,0 +1,92 @@
+﻿using eCommerceWeb.Controllers.Class;
+using eCommerceWeb.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using eCommerceWeb.Models.Entities;
+
+namespace eCommerceWeb.Controllers
+{
+	[Authorized]
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+
+        //Read
+        public ActionResult Read()
+        {
+            using (CategoriaModel model = new CategoriaModel())    //Abre conexão
+            {
+                List<Categoria> lista = model.Read();              //Usa conexão
+                ViewBag.lista = lista;
+            } //==> model.Dispose();                               //Fecha conexão
+
+            return View();
+        }
+
+        //Delete
+        public ActionResult Delete(int id)
+        {
+            //Efetuar a exclusão da Categoria
+            using (CategoriaModel model = new CategoriaModel())
+                model.Delete(id);
+                
+                return RedirectToAction("Read");
+        }
+
+        //Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            string nome = form["Nome"]; // <input type="text" name="Nome" ...
+
+
+            Categoria c = new Categoria();
+            c.Nome = nome;
+
+
+            using (CategoriaModel model = new CategoriaModel())
+            {
+                model.Create(c);
+            }
+
+            return RedirectToAction("Read");
+        }
+
+
+        //Update
+        public ActionResult Update(int id)
+        {
+            using (CategoriaModel model = new CategoriaModel())
+            {
+                ViewBag.categoria = model.Read(id);
+                return View();
+            }               
+        }
+
+        [HttpPost]
+        public ActionResult Update(FormCollection form)
+        {
+            
+            string nome = form["Nome"]; // <input type="text" name="Nome" ...
+            int id = int.Parse(form["CategoriaId"]);
+
+
+            Categoria c = new Categoria();
+            c.Nome = nome;
+            c.CategoriaId = id;
+
+            using (CategoriaModel model = new CategoriaModel())
+            {
+                model.Update(c);
+            }
+
+            return RedirectToAction("Read");
+        }
+    }
+}
---
+++ ClienteController.cs
@@ -0,0 +1,123 @@
+﻿using eCommerceWeb.Models.Entities;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using eCommerceWeb.Models;
+using eCommerceWeb.Controllers.Class;
+
+namespace eCommerceWeb.Controllers
+{
+
+	
+	public class ClienteController : Controller
+    {
+		// GET: Cliente
+		//TODO: Desenvolver as pages de create, update de Cliente
+		
+		public ActionResult Index()
+        {
+			ViewBag.NaoExiste = true;
+			return View();
+        }
+		[HttpPost]
+		public ActionResult Index(FormCollection form)
+		{
+			string email = form["Email"];
+			string senha = form["Senha"];
+
+			using (var model = new ClienteModel())
+			{
+				Cliente cli = model.Read(email, senha);
+				if (cli != null)
+				{
+					Session["Cliente"] = cli;
+					return RedirectToAction("Index", "HomeLogin");
+				}
+				else
+				{
+					ViewBag.NaoExiste = true;
+					return View();
+				}
+			}
+		}
+		public ActionResult Read()
+		{
+			using (ClienteModel model = new ClienteModel())    //Abre conexão
+			{
+				List<Cliente> lista = model.Read();              //Usa conexão
+				ViewBag.lista = lista;
+			} //==> model.Dispose();                               //Fecha conexão
+
+			return View();
+		}
+
+
+		[HttpGet]
+		public ActionResult Create()
+		{
+			using (var model = new ClienteModel())
+				ViewBag.Cliente = model.Read();
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Cliente p)
+		{
+			using (var model = new ClienteModel())
+			{
+				model.Create(p);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+
+
+
+
+		public ActionResult Delete(int id)
+		{
+			//Efetuar a exclusão da Categoria
+			using (ClienteModel model = new ClienteModel())
+				model.Delete(id);
+
+			return RedirectToAction("Read");
+		}
+
+		
+		
+		//Update
+		public ActionResult Update(int id)
+		{
+			using (ClienteModel model = new ClienteModel())
+			{
+				ViewBag.Cliente = model.Read(id);
+				return View();
+			}
+		}
+
+		[HttpPost]
+		public ActionResult Update(FormCollection form)
+		{
+
+			string nome = form["Nome"]; // <input type="text" name="Nome" ...
+			string email = form["Email"];
+			string senha = form["Senha"];
+			int id = int.Parse(form["ClienteID"]);
+
+
+			Cliente c = new Cliente();
+			c.Nome = nome;
+			c.Email = email;
+			c.Senha = senha;
+			c.ClienteID = id;
+
+			using (ClienteModel model = new ClienteModel())
+			{
+				model.Update(c);
+			}
+
+			return RedirectToAction("Read");
+		}
+
+	}
+}
