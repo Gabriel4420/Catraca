e5d3a85516ad22d81576a22bdbf7700502512dec
Gabriel4420 <37821374+Gabriel4420@users.noreply.github.com>
2018-10-06 15:20:38 -0300

    Add files via upload

---
+++ Authorized.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerceWeb.Controllers.Class
+{
+	public class Authorized:ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			if (filterContext.HttpContext.Session["Cliente"] == null)
+			{
+				filterContext.Result = new RedirectResult("/Cliente/Index");
+			}
+			base.OnActionExecuting(filterContext);
+		}
+	}
+}
